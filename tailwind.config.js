bf2ae7137925cdb071bd46db6bce228f55f5a2f1
Quentin <sastourne.quentin@hotmail.fr>
2022-11-23 11:29:21 +0100

    Add Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './public/index.html',
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+    './src/**/*.{html,js}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
